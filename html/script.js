ade12d5830379e3ac0b54a01c8c1cdced14d110d
FX. Bima Yudha Pratama <bimaboim8@gmail.com>
2024-08-27 22:26:06 +0700

    update | test file halaman

--- html/script.js
+++ html/script.js
@@ -1,1 +1,13 @@
 console.log("ini console di dalam file")
+
+async function hashString(message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+// Contoh penggunaan:
+hashString("Your message here").then(hash => console.log(hash));
